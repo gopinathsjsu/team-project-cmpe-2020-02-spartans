1896e9c21a159745c8981bb5472c1ad4b2680f85
Siddharth Kulkarni <siddharth.kulkarni98@gmail.com>
2024-12-04 02:33:57 -0800

    Add delete photo URL pattern

--- Backend/restaurant_review/restaurants/urls.py
+++ Backend/restaurant_review/restaurants/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RestaurantSearchView, RestaurantListView, RestaurantDetailView, AddRestaurantListingView, DeletePhotoView, PhotoDetailView, UploadPhotoView
+from .views import RestaurantSearchView, RestaurantListView, RestaurantDetailView, AddRestaurantListingView, DeletePhotoView, PhotoDetailView, UploadPhotoView, UpdateRestaurantListingView
 
 from reviews.views import GetReviewsView, SubmitReviewView
 urlpatterns = [
@@ -9,9 +9,11 @@ urlpatterns = [
     path('add/', AddRestaurantListingView.as_view(), name='add-restaurant'),
     path('<int:restaurant_id>/reviews/', GetReviewsView.as_view(), name='get-reviews'),
     path('<int:restaurant_id>/reviews/add/', SubmitReviewView.as_view(), name='submit-review'),
-    path('photos/<int:photo_id>/delete/', DeletePhotoView.as_view(), name='delete-photo'),
+    # path('photos/<int:photo_id>/delete/', DeletePhotoView.as_view(), name='delete-photo'),
     path('restaurants/<int:restaurant_id>/photos/upload/', UploadPhotoView.as_view(), name='upload-photo'),
     path('photos/<int:photo_id>/', PhotoDetailView.as_view(), name='photo-detail'),
+    path('<int:restaurant_id>/', UpdateRestaurantListingView.as_view(), name = 'edit-restaurant-detail'),
+    path('photos/<int:photo_id>/delete/', DeletePhotoView.as_view(), name='delete-restaurant-photo'),
 
 
 ]
