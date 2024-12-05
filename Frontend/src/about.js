889517f4926c6e62acb11b7647520ddb71cc2f77
F1804 <budhwani.faisal2802@gmail.com>
2024-12-05 01:57:20 -0800

    Update about.js

--- Frontend/src/about.js
+++ Frontend/src/about.js
@@ -103,7 +103,7 @@ function AboutPage() {
             </div>
             <div className="col-md-3">
               <div className="team-member">
-                <img src="https://via.placeholder.com/150" alt="Jane Smith" className="img-fluid rounded-circle" />
+                <img src={require('./Faisal.jpg')} alt="Jane Smith" className="img-fluid rounded-circle" />
                 <h4>FAISAL BUDHWANI</h4>
               </div>
             </div>
